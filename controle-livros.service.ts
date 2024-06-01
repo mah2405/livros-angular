186cfb85b6252dddf5f91a28751376514dd6fbd8
mah2405 <165704990+mah2405@users.noreply.github.com>
2024-06-01 11:27:17 -0300

    Update controle-livros.service.ts

--- controle-livros.service.ts
+++ controle-livros.service.ts
@@ -13,23 +13,23 @@ export class ControleLivrosService {
   {
     codigo: 1,
     codEditora: 101,
-    titulo: 'O Senhor dos Anéis',
-    resumo: 'Uma grande aventura épica...',
-    autores: ['J.R.R. Tolkien']
+    titulo: 'A garota do lago',
+    resumo: 'Summit Lake, uma pequena cidade entre montanhas, é esse tipo de lugar, bucólico e com encantadoras casas dispostas à beira de um longo trecho de água intocada.Duas semanas atrás, a estudante de direito Becca Eckersley foi brutalmente assassinada em uma dessas casas. Filha de um poderoso advogado, Becca estava no auge de sua vida. Atraída instintivamente pela notícia, a repórter Kelsey Castle vai até a cidade para investigar o caso. ...E LOGO SE ESTABELECE UMA CONEXÃO ÍNTIMA QUANDO UM VIVO CAMINHA NAS MESMAS PEGADAS DOS MORTOS...E enquanto descobre sobre as amizades de Becca, sua vida amorosa e os segredos que ela guardava, a repórter fica cada vez mais convencida de que a verdade sobre o que aconteceu com Becca pode ser a chave para superar as marcas sombrias de seu próprio passado..',
+    autores: ['Charlie Donlea']
 },
 {
     codigo: 2,
     codEditora: 102,
-    titulo: '1984',
-    resumo: 'Um clássico da distopia...',
-    autores: ['George Orwell']
+    titulo: 'Harry Potter e a Pedra Filosofal: 1',
+    resumo: 'Harry Potter é um garoto cujos pais, feiticeiros, foram assassinados por um poderosíssimo bruxo quando ele ainda era um bebê. Ele foi levado, então, para a casa dos tios que nada tinham a ver com o sobrenatural. Pelo contrário. Até os 10 anos, Harry foi uma espécie de gata borralheira: maltratado pelos tios, herdava roupas velhas do primo gorducho, tinha óculos remendados e era tratado como um estorvo. No dia de seu aniversário de 11 anos, entretanto, ele parece deslizar por um buraco sem fundo, como o de Alice no país das maravilhas, que o conduz a um mundo mágico. Descobre sua verdadeira história e seu destino: ser um aprendiz de feiticeiro até o dia em que terá que enfrentar a pior força do mal, o homem que assassinou seus pais. O menino de olhos verde, magricela e desengonçado, tão habituado à rejeição, descobre, também, que é um herói no universo dos magos. Potter fica sabendo que é a única pessoa a ter sobrevivido a um ataque do tal bruxo do mal e essa é a causa da marca em forma de raio que ele carrega na testa. Ele não é um garoto qualquer, ele sequer é um feiticeiro qualquer ele é Harry Potter, símbolo de poder, resistência e um líder natural entre os sobrenaturais. A fábula, recheada de fantasmas, paredes que falam, caldeirões, sapos, unicórnios, dragões e gigantes, não é, entretanto, apenas um passatempo.',
+    autores: ['J.K. Rowling']
 },
 {
     codigo: 3,
     codEditora: 103,
-    titulo: 'Dom Quixote',
-    resumo: 'As aventuras do cavaleiro...',
-    autores: ['Miguel de Cervantes']
+    titulo: 'É Assim que Acaba: 1',
+    resumo: 'Em É assim que acaba , Colleen Hoover nos apresenta Lily, uma jovem que se mudou de uma cidadezinha do Maine para Boston, se formou em marketing e abriu a própria floricultura. E é em um dos terraços de Boston que ela conhece Ryle, um neurocirurgião confiante, teimoso e talvez até um pouco arrogante, com uma grande aversão a relacionamentos, mas que se sente muito atraído por ela....',
+    autores: ['Colleen Hoover']
      },
    ]
   }
